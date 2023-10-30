b01afd41fd0afba42581eb573bcf4bb9c12e57f3
Daniel Hackworth <dan.hackworth.515@gmail.com>
2023-10-30 18:27:25 -0400

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
@@ -6,6 +6,61 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  const onChangeType = (e) => {
+    setFilters({type: e.target.value})
+  }
+  
+
+useEffect( ()=>{
+  onFindPetsClick()
+},[])
+
+
+const onFindPetsClick = () => {
+  let queryStr =''
+  if (filters.type !== 'all'){
+    queryStr = `?type=${filters.type}`
+  }
+  fetch(`http://localhost:3001/pets${queryStr}`)
+  .then(resp=>resp.json())
+  .then((data)=>{
+    setPets(data)
+    
+  })
+}
+
+
+
+const onAdoptPet = (id) => {
+  
+  let  updatedPets = pets.map(pet=>{
+    if(pet.id===id){
+       pet.isAdopted = true
+    }
+    return pet
+  })
+  setPets(updatedPets)
+
+  
+  // fetch(`http://localhost:3001/pets/${id}`,{
+  //   method:'PATCH',
+  //   headers:{
+  //     'Content-Type': 'application/json'
+  //   },
+  //   body: JSON.stringify({"isAdopted":true})
+  //   })
+  //   .then(resp=>resp.json())
+  //   .then(data=>{
+      
+        
+        
+  //   })   
+  }
+  
+
+
+
 
   return (
     <div className="ui container">
@@ -15,10 +70,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet} />
           </div>
         </div>
       </div>
